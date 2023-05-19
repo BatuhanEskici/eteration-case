21f1ec24db789224ac7726b652f96a2973674df2
Batuhan Eskici <batuhan_eskici@hotmail.com>
2023-05-19 11:34:39 +0300

    add product listing page layout

--- src/components/ProductListingPage/ProductListingPage.js
+++ src/components/ProductListingPage/ProductListingPage.js
@@ -1,7 +1,15 @@
+import Sort from '../Filters/Sort/Sort';
+import ProductCard from '../ProductCard/ProductCard'
+import Checkout from '../Checkout/Checkout';
+
 export default function ProductListingPage() {
     return (
-        <div>
-            PLP
+        <div className='flex items-center w-4/5 mx-auto mt-6'>
+            <div className='w-1/3'><Sort /></div>
+
+            <div className='w-2/3'><ProductCard /></div>
+
+            <div className='w-1/3'><Checkout /></div>
         </div>
     )
 }
