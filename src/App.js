fa581592b383eac2e56a1ce2271d66e880723554
Batuhan Eskici <batuhan_eskici@hotmail.com>
2023-05-19 23:53:32 +0300

    add axios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
+import axios from 'axios';
+import { useEffect } from 'react';
 import Navbar from './components/Navbar/Navbar'
 import ProductListingPage from './components/ProductListingPage/ProductListingPage';
 
 function App() {
+  useEffect(() => {
+    axios.get('https://5fc9346b2af77700165ae514.mockapi.io/products')
+    .then((response) => {
+      console.log(response);
+    })
+  }, [])
+
   return (
     <div className='bg-[#F9F9F9] h-screen'>
       <Navbar />
