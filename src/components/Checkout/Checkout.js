d2bb3419c9d4a98fc9a812c087db85be34d7e1fd
Batuhan Eskici <batuhan_eskici@hotmail.com>
2023-05-23 01:23:40 +0300

    add toLocaleString to checkout

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -10,7 +10,7 @@ export default function Checkout({ className }) {
     <div className={`${className} bg-white p-3 shadow-lg`}>
       Total Price:
       <span className="text-[#2A59FE] font-bold">
-        &nbsp;{context.totalPrice} TL
+        &nbsp;{context.totalPrice.toLocaleString()} TL
       </span>
       <button className="bg-[#2A59FE] text-white px-3 py-1 rounded w-full text-center mt-3">
         Checkout
